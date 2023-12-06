cbad33bbc361ca4e71bf7915aa6cb4f575cc1990
kjersgarde <102928366+kjersgarde@users.noreply.github.com>
2023-12-06 16:21:50 -0500

    Update dec-6-q1.py

--- dec-6-q1.py
+++ dec-6-q1.py
@@ -1,2 +1,14 @@
-with open('day1input.txt', 'r') as f:
-  for line in f:
+with open('dec-6-input-1.txt', 'r') as f:
+    total = 0
+    for line in f:
+        concatenatedString = ''
+        finalString = ''
+        for char in line:
+            if char.isdigit():
+                concatenatedString += char
+        finalString += concatenatedString[0]
+        finalString += concatenatedString[-1]
+        curNum = int(finalString)
+        total += curNum
+        print("Total is:",total, "And the Number is:",curNum)
+    print(total)
